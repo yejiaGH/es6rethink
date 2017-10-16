2250872d3b9aa5e64afe8f696d394dfcde13f634
yejia <ye.jia.alice@gmail.com>
2017-10-16 23:38:24 +0800

    长轮询

--- app/js/class/lesson15.js
+++ app/js/class/lesson15.js
@@ -70,18 +70,74 @@
  * Try more examples
  */
 
+// {
+//   function timeout(ms) {
+//     return new Promise((resolve) => {
+//       setTimeout(resolve, ms);
+//     });
+//   }
+  
+//   async function asyncPrint(value, ms) {
+//     await timeout(ms);
+//     console.log(value,ms);
+//   }
+  
+//   asyncPrint('hello world', 50);
+// }
+
 {
-  function timeout(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
+  let draw = function(count) {
+    // Business Logic Code
+    console.info(`剩余${count}次`);
   }
-  
-  async function asyncPrint(value, ms) {
-    await timeout(ms);
-    console.log(value,ms);
+
+  let residue = function* (count) {
+    while(count > 0) {
+      count--;
+      yield draw(count);
+    }
   }
-  
-  asyncPrint('hello world', 50);
+
+  let star = residue(5);
+  let btn = document.createElement('button');
+  btn.id = 'start';
+  btn.textContent = '抽奖哈哈哈';
+  document.body.appendChild(btn);
+  document.getElementById('start').addEventListener('click', function(){
+    star.next();
+  }, false);
 }
 
+{
+  // 长轮询
+  let ajax = function* (callCount) {
+    yield new Promise(function(resolve, reject) {
+      console.log('call api');
+      setTimeout(function() {
+        if(callCount > 2) {
+          resolve({code:0});
+        } else {
+          resolve({code:-1});
+        }        
+      }, 200);
+    })
+  }
+  
+  let pull = function() {
+    callCount++;
+    let generator = ajax(callCount);
+    let step = generator.next();
+    step.value.then(function(d) {
+      if(d.code!=0) {
+        setTimeout(function () {
+          console.log('wait');
+          pull();
+        }, 1000);
+      } else {
+        console.log(d);
+      }
+    })
+  }
+  let callCount = 0;
+  pull();
+}
